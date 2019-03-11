9fc2a9f5958bb1e98c23bd13b3cb38cf14b61707
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-11 22:26:28 +0100

    Update HelloTest.java

--- src/test/java/io/github/oliviercailloux/sample_jax_rs/HelloTest.java
+++ src/test/java/io/github/oliviercailloux/sample_jax_rs/HelloTest.java
@@ -7,10 +7,10 @@ import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
 public class HelloTest {
-
+/*
 	@SuppressWarnings("unused")
 	private static final Logger LOGGER = Logger.getLogger(HelloTest.class.getCanonicalName());
-
+*/
 	@Test
 	public void testServlet() throws Exception {
 		assertEquals("Hello, world.", new Hello().justSayHello());
